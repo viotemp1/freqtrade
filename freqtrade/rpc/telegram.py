b0a25132f820dcd71f579a2ec0b75c6318d11a95
Viorel Ublea <34300519+viotemp1@users.noreply.github.com>
2024-05-05 22:46:01 +0300

    my

--- freqtrade/rpc/telegram.py
+++ freqtrade/rpc/telegram.py
@@ -776,7 +776,7 @@ class Telegram(RPCHandler):
                 message = "\n".join(lines[:-1] + [lines[1]] + [lines[-1]])
             final_message = f"<pre>{message}</pre>" + "\n"
             for pair in pairlist:
-                final_message +=  f"<a href='https://www.tradingview.com/symbols/{str(pair).replace('/','')}/'>{pair\t}</a>"
+                final_message +=  f"<a href='https://www.tradingview.com/symbols/{str(pair).replace('/','')}/'>{pair}&emsp;</a>"
             
             await self._send_msg(final_message, parse_mode=ParseMode.HTML,
                                  reload_able=True, callback_path="update_status_table",

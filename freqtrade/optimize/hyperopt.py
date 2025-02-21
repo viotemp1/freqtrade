cd607c7016c4a556ac8fa9a2b56c23b11ad18db6
viotemp1 <mr_vii@icloud.com>
2025-02-21 12:32:31 +0200

    my

--- freqtrade/optimize/hyperopt.py
+++ freqtrade/optimize/hyperopt.py
@@ -723,10 +723,12 @@ class Hyperopt:
                         optuna__sampler = optuna.samplers.TPESampler(
                             seed=self.random_state
                         )
-                    searcher_algo = tune.create_searcher(
-                        searcher,
-                        sampler=optuna__sampler,
-                    )
+                    with warnings.catch_warnings():
+                        warnings.simplefilter("ignore", optuna.exceptions.ExperimentalWarning)
+                        searcher_algo = tune.create_searcher(
+                            searcher,
+                            sampler=optuna__sampler,
+                        )
             elif (
                 searcher == "hebo"
             ):  # gp gpy gpy_mlp psgld svidkl deep_ensemble rf catboost svgp mcbn masked_deep_ensemble fe_deep_ensemble gumbel

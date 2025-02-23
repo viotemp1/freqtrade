4afa734ca9552bf04ecbef23671282e9fcb1ebbf
viotemp1 <mr_vii@icloud.com>
2025-02-23 16:25:52 +0200

    my

--- freqtrade/optimize/hyperopt/hyperopt_optimizer.py
+++ freqtrade/optimize/hyperopt/hyperopt_optimizer.py
@@ -76,7 +76,7 @@ from optuna.exceptions import ExperimentalWarning
 
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=FutureWarning)
-    warnings.filterwarnings("ignore", ExperimentalWarning)
+    warnings.filterwarnings("ignore", category=ExperimentalWarning)
     from skopt import Optimizer
     from skopt.space import Dimension
     import ray
@@ -115,7 +115,7 @@ def ray_setup_func():
     from optuna.exceptions import ExperimentalWarning
 
     with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", ExperimentalWarning)
+        warnings.filterwarnings("ignore", category=ExperimentalWarning)
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
 
@@ -698,7 +698,7 @@ class Hyperopt:
                 from optuna.exceptions import ExperimentalWarning
 
                 with warnings.catch_warnings():
-                    warnings.filterwarnings("ignore", ExperimentalWarning)
+                    warnings.filterwarnings("ignore", category=ExperimentalWarning)
                     import optuna
 
                     # TPESampler NSGAIIISampler CmaEsSampler GPSampler NSGAIISampler QMCSampler
